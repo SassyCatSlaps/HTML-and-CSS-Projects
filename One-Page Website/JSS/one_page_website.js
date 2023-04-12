29174449f237c8ca6ba82979da3969764c3ac390
Viktoriya (Vik) <127991377+SassyCatSlaps@users.noreply.github.com>
2023-04-11 22:59:56 -0600

    ONE-PAGE WEBSITE SUBMISSION ASSIGNMENT
    Page has a working modal image gallery.
    
    scrapped heaps on this wp = was rubbish. saved scraps in folder. form and its css need a redo. would be easiest to nuke all old stuff and start over. the color scheme pains me

---
+++ One-Page Website/JSS/one_page_website.js
@@ -0,0 +1,39 @@
+//THIS CODE IS FOR THE LIGHTBOX MODAL
+//open modal
+function openModal() {
+    document.getElementById("vikModal").style.display = "block";
+}
+//close modal
+function closeModal() {
+    document.getElementById("vikModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+//forward/back controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+//sml img controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("vikSlides");
+    var dots = document.getElementsByClassName("controlSml");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt; 
+}
